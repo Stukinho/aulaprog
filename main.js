b1118ac945f0fb5fef5ae1fc0faf66ed776a61f4
Stukinho <142433899+Stukinho@users.noreply.github.com>
2024-09-23 12:13:44 -0300

    .

--- main.js
+++ main.js
@@ -12,6 +12,17 @@ let areaTriangulo = function(a,b){
     return a*b/2;
 }
 console.log(areaTriangulo);
-let nome = prompt(?)
-document.write (nome);
-parseInt(prompt('?'))
+
+let numero1 = parseFloat(prompt('Digite o primeiro número:'));
+let numero2 = parseFloat(prompt('Digite o segundo número:'));
+let numero3 = parseFloat(prompt('Digite o terceiro número:'));
+
+let media = (numero1 + numero2 + numero3) / 3;
+
+let resultadoDiv = document.getElementById('resultado');
+resultadoDiv.innerHTML = `
+    Você digitou:<br>
+    Primeiro número: ${numero1}<br>
+    Segundo número: ${numero2}<br>
+    Terceiro número: ${numero3}<br>
+    A média dos números é: ${media}
